8871d117045d1c1b76f393f7d9ace074e5f1b12c
Gemma Whitfield <70214774+gemmawhitfield@users.noreply.github.com>
2021-02-04 14:01:53 +0000

    Create madlibs.js

---
+++ madlibs.js
@@ -0,0 +1,13 @@
+// This is my first JavaScript mad-libs game, created from scratch! 
+
+// Create variables
+var myNoun = "egg";
+var myVerb = "gallop";
+var myAdjective = "blue";
+var myAdverb = "excited";
+
+// Write the story, concatenating the variables 
+var myStory = "The " + myNoun + " started to " + myVerb + " down the street.\nIt was very " + myAdverb + " and it was turning " + myAdjective + "!"
+
+// Print the story! 
+console.log(myStory)
